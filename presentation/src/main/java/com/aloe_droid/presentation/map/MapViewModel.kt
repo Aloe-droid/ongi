2aaf3124675009aeffd928887b0828b6984882fa
Aloe-droid <https://github.com/Aloe-droid>
2025-04-23 01:17:55 +0900

    feat: Presentation Layer Map 기본 화면 추가

--- presentation/src/main/java/com/aloe_droid/presentation/map/MapViewModel.kt
+++ presentation/src/main/java/com/aloe_droid/presentation/map/MapViewModel.kt
@@ -1,12 +1,9 @@
 package com.aloe_droid.presentation.map
 
 import androidx.lifecycle.SavedStateHandle
-import androidx.navigation.toRoute
 import com.aloe_droid.presentation.base.BaseViewModel
-import com.aloe_droid.presentation.base.UiContract.State
 import com.aloe_droid.presentation.map.contract.effect.MapEffect
 import com.aloe_droid.presentation.map.contract.event.MapEvent
-import com.aloe_droid.presentation.map.contract.route.Map
 import com.aloe_droid.presentation.map.contract.state.MapUiState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
@@ -16,9 +13,8 @@ class MapViewModel @Inject constructor(
     savedStateHandle: SavedStateHandle
 ) : BaseViewModel<MapUiState, MapEvent, MapEffect>(savedStateHandle) {
 
-    override fun initialState(savedStateHandle: SavedStateHandle): State<MapUiState> {
-        val map: Map = savedStateHandle.toRoute()
-        return State.Idle
+    override fun initState(savedStateHandle: SavedStateHandle): MapUiState {
+        return MapUiState
     }
 
     override fun handleEvent(event: MapEvent) = when {
--- presentation/src/main/java/com/aloe_droid/presentation/map/contract/state/MapUiState.kt
+++ presentation/src/main/java/com/aloe_droid/presentation/map/contract/state/MapUiState.kt
@@ -1,6 +1,7 @@
 package com.aloe_droid.presentation.map.contract.state
 
 import androidx.compose.runtime.Stable
+import com.aloe_droid.presentation.base.UiContract
 
 @Stable
-data object MapUiState
+data object MapUiState : UiContract.State
