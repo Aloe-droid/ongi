ecff2f36e680016ac82bb5b6d50be2f481f64268
Aloe-droid <https://github.com/Aloe-droid>
2025-04-23 01:18:37 +0900

    feat: Presentation Layer Setting 기본 화면 추가

--- presentation/src/main/java/com/aloe_droid/presentation/setting/SettingViewModel.kt
+++ presentation/src/main/java/com/aloe_droid/presentation/setting/SettingViewModel.kt
@@ -1,12 +1,9 @@
 package com.aloe_droid.presentation.setting
 
 import androidx.lifecycle.SavedStateHandle
-import androidx.navigation.toRoute
 import com.aloe_droid.presentation.base.BaseViewModel
-import com.aloe_droid.presentation.base.UiContract.State
 import com.aloe_droid.presentation.setting.contract.effect.SettingEffect
 import com.aloe_droid.presentation.setting.contract.event.SettingEvent
-import com.aloe_droid.presentation.setting.contract.route.Setting
 import com.aloe_droid.presentation.setting.contract.state.SettingUiState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
@@ -16,9 +13,8 @@ class SettingViewModel @Inject constructor(
     savedStateHandle: SavedStateHandle
 ) : BaseViewModel<SettingUiState, SettingEvent, SettingEffect>(savedStateHandle) {
 
-    override fun initialState(savedStateHandle: SavedStateHandle): State<SettingUiState> {
-        val setting: Setting = savedStateHandle.toRoute()
-        return State.Idle
+    override fun initState(savedStateHandle: SavedStateHandle): SettingUiState {
+        return SettingUiState
     }
 
     override fun handleEvent(event: SettingEvent) = when {
--- presentation/src/main/java/com/aloe_droid/presentation/setting/contract/state/SettingUiState.kt
+++ presentation/src/main/java/com/aloe_droid/presentation/setting/contract/state/SettingUiState.kt
@@ -1,6 +1,7 @@
 package com.aloe_droid.presentation.setting.contract.state
 
 import androidx.compose.runtime.Stable
+import com.aloe_droid.presentation.base.UiContract
 
 @Stable
-data object SettingUiState
+data object SettingUiState : UiContract.State
