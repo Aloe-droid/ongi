93122b1edbdcdf93c2bce975237098d73b208751
Aloe-droid <https://github.com/Aloe-droid>
2025-04-23 01:16:50 +0900

    feat: Presentation Layer BaseViewModel 과 Base Contract 추가

--- presentation/src/main/java/com/aloe_droid/presentation/base/BaseViewModel.kt
+++ presentation/src/main/java/com/aloe_droid/presentation/base/BaseViewModel.kt
@@ -6,14 +6,15 @@ import androidx.lifecycle.viewModelScope
 import com.aloe_droid.presentation.base.UiContract.Event
 import com.aloe_droid.presentation.base.UiContract.SideEffect
 import com.aloe_droid.presentation.base.UiContract.State
-import com.aloe_droid.presentation.base.flow.RefreshableUiStateFlow
+import com.aloe_droid.presentation.base.flow.RefreshableFlow
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.channels.ChannelResult
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.catch
-import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.flow.retryWhen
 import kotlinx.coroutines.flow.stateIn
@@ -21,32 +22,39 @@ import kotlinx.coroutines.launch
 import timber.log.Timber
 import java.io.IOException
 
-abstract class BaseViewModel<UiState, UiEvent : Event, UiEffect : SideEffect>(
+abstract class BaseViewModel<UiState : State, UiEvent : Event, UiEffect : SideEffect>(
     savedStateHandle: SavedStateHandle
 ) : ViewModel() {
 
-    private val initState: State<UiState> by lazy { initialState(savedStateHandle) }
-    private val _uiState = RefreshableUiStateFlow<UiState>(initState)
-    val uiState: StateFlow<State<UiState>> = _uiState.flow.stateIn(
-        scope = viewModelScope,
-        started = SharingStarted.WhileSubscribed(STATE_FLOW_TIME_OUT),
-        initialValue = initState
-    )
+    private val initialState: UiState by lazy { initState(savedStateHandle) }
+    private val _uiState by lazy { RefreshableFlow(initialState) }
+    val uiState: StateFlow<UiState> by lazy {
+        _uiState.flow.stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(STATE_FLOW_TIME_OUT),
+            initialValue = initialState
+        )
+    }
+
+    val currentState: UiState get() = uiState.value
 
     private val _uiEffect: Channel<UiEffect> = Channel(Channel.BUFFERED)
     val uiEffect: Flow<UiEffect> = _uiEffect.receiveAsFlow()
-
-    abstract fun initialState(savedStateHandle: SavedStateHandle): State<UiState>
-    abstract fun handleEvent(event: UiEvent)
 
     fun sendEvent(event: UiEvent) = viewModelScope.safeLaunch {
         handleEvent(event)
     }
 
-    protected fun updateState(reducer: (State<UiState>) -> State<UiState>) =
-        _uiState.update(reduce = reducer)
+    protected fun sendSideEffect(uiEffect: UiEffect) {
+        val result: ChannelResult<Unit> = _uiEffect.trySend(uiEffect)
+        if (result.isFailure) Timber.e(result.exceptionOrNull())
+    }
 
-    protected fun refreshState() = viewModelScope.safeLaunch {
+    protected fun updateState(function: (UiState) -> UiState) {
+        _uiState.update(function = function)
+    }
+
+    protected suspend fun refresh() {
         _uiState.refresh()
     }
 
@@ -59,46 +67,34 @@ abstract class BaseViewModel<UiState, UiEvent : Event, UiEffect : SideEffect>(
             .onFailure { throwable: Throwable -> handleError(throwable = throwable) }
     }
 
-    protected suspend fun Flow<UiState>.collectWithUiState() = onStart {
-        handleLoading()
-    }.retryWhen { throwable: Throwable, attempt: Long ->
-        handleRetry(throwable = throwable, attempt = attempt)
-    }.catch { throwable: Throwable ->
-        handleError(throwable = throwable)
-    }.collect { data: UiState ->
-        handleSuccess(data = data)
-    }
-
-    private fun handleError(throwable: Throwable) {
-        Timber.e(t = throwable)
-        updateState { state: State<UiState> ->
-            State.Error<UiState>(data = state.getData(), throwable = throwable)
+    protected suspend fun <T> Flow<T>.safeCollect(collector: suspend (T) -> Unit) =
+        retryWhen { throwable: Throwable, attempt: Long ->
+            handleRetry(throwable = throwable, attempt = attempt)
+        }.catch { throwable: Throwable ->
+            handleError(throwable = throwable)
+        }.collect { uiState: T ->
+            collector(uiState)
         }
+
+    open fun handleError(throwable: Throwable) {
+        Timber.e(t = throwable)
     }
 
-    private fun handleLoading() = updateState { state: State<UiState> ->
-        State.Loading<UiState>(data = state.getData())
-    }
-
-    private fun handleSuccess(data: UiState) = updateState {
-        State.Success<UiState>(data = data)
-    }
-
-    private fun handleRetry(throwable: Throwable, attempt: Long): Boolean {
+    open suspend fun handleRetry(throwable: Throwable, attempt: Long): Boolean {
         if (attempt >= MAX_RETRY) return false
         if (throwable !is IOException) return false
+
+        delay(RETRY_INTERVAL)
         return true
     }
 
-    private fun State<UiState>.getData(): UiState? = when (this) {
-        is State.Idle -> null
-        is State.Loading -> data
-        is State.Success -> data
-        is State.Error -> data
-    }
+    abstract fun initState(savedStateHandle: SavedStateHandle): UiState
+
+    abstract fun handleEvent(event: UiEvent)
 
     companion object {
         private const val MAX_RETRY: Int = 2
         private const val STATE_FLOW_TIME_OUT: Long = 5_000L
+        private const val RETRY_INTERVAL: Long = 1_000L
     }
 }
