a3d64452e6984815a0c049c3e374224791820346
Aloe-droid <https://github.com/Aloe-droid>
2025-05-01 15:57:36 +0900

    feat: Presentation FilteredStore 구현

---
+++ presentation/src/main/java/com/aloe_droid/presentation/filtered_store/FilteredStoreNavigation.kt
@@ -0,0 +1,90 @@
+package com.aloe_droid.presentation.filtered_store
+
+import androidx.compose.material3.SnackbarVisuals
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.res.stringResource
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.compose.composable
+import androidx.paging.LoadState
+import androidx.paging.compose.LazyPagingItems
+import androidx.paging.compose.collectAsLazyPagingItems
+import com.aloe_droid.presentation.R
+import com.aloe_droid.presentation.base.component.LoadingScreen
+import com.aloe_droid.presentation.base.view.BaseSnackBarVisuals
+import com.aloe_droid.presentation.base.view.CollectSideEffects
+import com.aloe_droid.presentation.filtered_store.contract.FilteredStore
+import com.aloe_droid.presentation.filtered_store.contract.FilteredStoreEffect
+import com.aloe_droid.presentation.filtered_store.contract.FilteredStoreEvent
+import com.aloe_droid.presentation.filtered_store.contract.FilteredStoreUiState
+import com.aloe_droid.presentation.filtered_store.data.StoreDistanceRange
+import com.aloe_droid.presentation.filtered_store.data.StoreFilterNavTypes.StoreFilterTypeMap
+import com.aloe_droid.presentation.filtered_store.data.StoreSortType
+import com.aloe_droid.presentation.home.data.StoreData
+import java.util.UUID
+
+fun NavGraphBuilder.filteredStoreScreen(
+    showSnackMessage: (SnackbarVisuals) -> Unit,
+    navigateToStore: (UUID) -> Unit,
+) = composable<FilteredStore>(typeMap = StoreFilterTypeMap) {
+
+    val viewModel: FilteredStoreViewModel = hiltViewModel()
+    val uiState: FilteredStoreUiState by viewModel.uiState.collectAsStateWithLifecycle()
+    val storeItems: LazyPagingItems<StoreData> = viewModel.pagingDataFlow.collectAsLazyPagingItems()
+
+    CollectSideEffects(effectFlow = viewModel.uiEffect) { sideEffect: FilteredStoreEffect ->
+        when (sideEffect) {
+            is FilteredStoreEffect.ShowErrorMessage -> {
+                val snackBarVisuals = BaseSnackBarVisuals(message = sideEffect.message)
+                showSnackMessage(snackBarVisuals)
+            }
+
+            is FilteredStoreEffect.NavigateStore -> {
+                navigateToStore(sideEffect.id)
+            }
+        }
+    }
+
+    if (storeItems.loadState == LoadState.Loading) {
+        LoadingScreen(content = stringResource(id = R.string.loading))
+    } else {
+        FilteredStoreScreen(
+            storeItems = storeItems,
+            isRefresh = uiState.isRefresh,
+            selectedSortType = uiState.storeFilter.sortType,
+            selectedDistanceRange = uiState.storeFilter.distanceRange,
+            isShowOrderBottomSheet = uiState.isShowOrderBottomSheet,
+            isShowDistanceBottomSheet = uiState.isShowDistanceBottomSheet,
+            selectStore = { storeData: StoreData ->
+                val event = FilteredStoreEvent.SelectStore(storeData = storeData)
+                viewModel.sendEvent(event = event)
+            },
+            onRefresh = {
+                val event = FilteredStoreEvent.RefreshEvent
+                viewModel.sendEvent(event = event)
+            },
+            setShowOrderBottomSheet = {
+                val event = FilteredStoreEvent.ShowOrderBottomSheet
+                viewModel.sendEvent(event = event)
+            },
+            setShowDistanceBottomSheet = {
+                val event = FilteredStoreEvent.ShowDistanceBottomSheet
+                viewModel.sendEvent(event = event)
+            },
+            onDismissBottomSheet = {
+                val event = FilteredStoreEvent.CloseBottomSheet
+                viewModel.sendEvent(event = event)
+            },
+            onSelectSortType = { sortType: StoreSortType ->
+                val event = FilteredStoreEvent.SelectStoreSortType(sortType = sortType)
+                viewModel.sendEvent(event = event)
+            },
+            onSelectDistanceRange = { distanceRange: StoreDistanceRange ->
+                val event = FilteredStoreEvent.SelectDistanceRange(distanceRange = distanceRange)
+                viewModel.sendEvent(event = event)
+            },
+        )
+    }
+
+}
---
+++ presentation/src/main/java/com/aloe_droid/presentation/filtered_store/FilteredStoreScreen.kt
@@ -0,0 +1,158 @@
+package com.aloe_droid.presentation.filtered_store
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.paging.PagingData
+import androidx.paging.compose.LazyPagingItems
+import androidx.paging.compose.collectAsLazyPagingItems
+import com.aloe_droid.presentation.R
+import com.aloe_droid.presentation.base.component.OnGiRefreshBox
+import com.aloe_droid.presentation.base.component.StoreInfo
+import com.aloe_droid.presentation.base.theme.DefaultPadding
+import com.aloe_droid.presentation.base.theme.LargePadding
+import com.aloe_droid.presentation.base.theme.toDistanceString
+import com.aloe_droid.presentation.filtered_store.component.FilterBottomSheet
+import com.aloe_droid.presentation.filtered_store.component.FilterRow
+import com.aloe_droid.presentation.filtered_store.data.StoreDistanceRange
+import com.aloe_droid.presentation.filtered_store.data.StoreSortType
+import com.aloe_droid.presentation.home.data.StoreData
+import kotlinx.coroutines.flow.flowOf
+import java.util.UUID
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FilteredStoreScreen(
+    modifier: Modifier = Modifier,
+    isRefresh: Boolean,
+    isShowOrderBottomSheet: Boolean,
+    isShowDistanceBottomSheet: Boolean,
+    selectedSortType: StoreSortType,
+    selectedDistanceRange: StoreDistanceRange,
+    storeItems: LazyPagingItems<StoreData>,
+    selectStore: (StoreData) -> Unit,
+    setShowOrderBottomSheet: () -> Unit,
+    setShowDistanceBottomSheet: () -> Unit,
+    onSelectSortType: (StoreSortType) -> Unit,
+    onSelectDistanceRange: (StoreDistanceRange) -> Unit,
+    onDismissBottomSheet: () -> Unit,
+    onRefresh: () -> Unit,
+) {
+    OnGiRefreshBox(
+        modifier = modifier.padding(horizontal = LargePadding),
+        isRefreshing = isRefresh,
+        onRefresh = onRefresh,
+    ) {
+        Column {
+            FilterRow(
+                modifier = Modifier
+                    .padding(vertical = DefaultPadding)
+                    .fillMaxWidth(),
+                order = stringResource(id = selectedSortType.getNameRes()),
+                setShowOrderBottomSheet = setShowOrderBottomSheet,
+                distance = selectedDistanceRange.toDistanceString(),
+                setShowDistanceBottomSheet = setShowDistanceBottomSheet
+            )
+
+            if (!storeItems.loadState.isIdle || storeItems.itemCount != 0) {
+                LazyColumn(modifier = Modifier.fillMaxSize()) {
+                    items(storeItems.itemSnapshotList.items, key = { it.id }) { data ->
+                        StoreInfo(
+                            modifier = Modifier.fillMaxWidth(),
+                            imageUrl = data.imageUrl ?: "",
+                            storeName = data.name,
+                            storeAddress = "${data.city} ${data.district}",
+                            distance = data.distance,
+                            favoriteCount = data.favoriteCount,
+                            onClick = { selectStore(data) }
+                        )
+                        Spacer(modifier = Modifier.height(height = LargePadding))
+                    }
+                }
+            } else {
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(vertical = DefaultPadding),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Spacer(modifier = Modifier.weight(1f))
+
+                    Image(
+                        modifier = Modifier.size(200.dp),
+                        painter = painterResource(id = R.drawable.no_store),
+                        contentDescription = null
+                    )
+                    Spacer(modifier = Modifier.height(16.dp))
+                    Text(
+                        text = stringResource(id = R.string.no_store),
+                        style = MaterialTheme.typography.titleMedium,
+                        textAlign = TextAlign.Center
+                    )
+
+                    Spacer(modifier = Modifier.weight(2f))
+                }
+            }
+        }
+    }
+
+    FilterBottomSheet(
+        isShowOrderBottomSheet = isShowOrderBottomSheet,
+        isShowDistanceBottomSheet = isShowDistanceBottomSheet,
+        onDismissBottomSheet = onDismissBottomSheet,
+        selectedSortType = selectedSortType,
+        onSelectSortType = onSelectSortType,
+        selectedDistanceRange = selectedDistanceRange,
+        onSelectDistanceRange = onSelectDistanceRange
+    )
+}
+
+@Composable
+@Preview
+fun FilteredStoreScreenPreview() {
+    val list = List(10) {
+        StoreData(
+            id = UUID.randomUUID(),
+            name = "상호명",
+            city = "시도",
+            district = "시군구",
+            distance = 4.5,
+            favoriteCount = 51,
+            imageUrl = null
+        )
+    }
+    val fakeFlow = flowOf(PagingData.from(list)).collectAsLazyPagingItems()
+    FilteredStoreScreen(
+        isRefresh = false,
+        isShowOrderBottomSheet = false,
+        isShowDistanceBottomSheet = false,
+        storeItems = fakeFlow,
+        selectedSortType = StoreSortType.DISTANCE,
+        selectedDistanceRange = StoreDistanceRange.K_10,
+        selectStore = {},
+        onRefresh = {},
+        setShowOrderBottomSheet = {},
+        setShowDistanceBottomSheet = {},
+        onDismissBottomSheet = {},
+        onSelectSortType = {},
+        onSelectDistanceRange = {}
+    )
+}
