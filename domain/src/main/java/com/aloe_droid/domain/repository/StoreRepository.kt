2931c1ca97a478d8b4b6a2a5a3e4d5794d9c16c9
Aloe-droid <https://github.com/Aloe-droid>
2025-05-01 15:24:06 +0900

    chore: Domain Store 페이징 정의

--- domain/src/main/java/com/aloe_droid/domain/repository/StoreRepository.kt
+++ domain/src/main/java/com/aloe_droid/domain/repository/StoreRepository.kt
@@ -1,9 +1,12 @@
 package com.aloe_droid.domain.repository
 
+import androidx.paging.PagingData
 import com.aloe_droid.domain.entity.Store
 import com.aloe_droid.domain.entity.StoreQuery
 import kotlinx.coroutines.flow.Flow
 
 interface StoreRepository {
     fun getStoreList(storeQuery: StoreQuery): Flow<List<Store>>
+
+    fun getStoreStream(storeQuery: StoreQuery) : Flow<PagingData<Store>>
 }
