08ee91871aa9c17c63841df91c4dafe9423e452d
Aloe-droid <https://github.com/Aloe-droid>
2025-05-01 15:43:48 +0900

    feat: Presentation store 데이터 삭제

--- presentation/src/main/java/com/aloe_droid/presentation/store/data/StoreFilter.kt
+++
@@ -1,8 +0,0 @@
-package com.aloe_droid.presentation.store.data
-
-data class StoreFilter (
-    val category: StoreCategory = StoreCategory.NONE,
-    val sortType: StoreSortType = StoreSortType.FAVORITE,
-    val distanceRange: StoreDistanceRange = StoreDistanceRange.NONE,
-    val searchQuery: String = ""
-)
