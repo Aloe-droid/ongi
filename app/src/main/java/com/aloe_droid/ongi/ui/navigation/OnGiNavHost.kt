13ebd538dc0db83926160a5bddca7eb244176368
Aloe-droid <https://github.com/Aloe-droid>
2025-05-01 15:26:12 +0900

    feat: App screen navigation 연동

--- app/src/main/java/com/aloe_droid/ongi/ui/navigation/OnGiNavHost.kt
+++ app/src/main/java/com/aloe_droid/ongi/ui/navigation/OnGiNavHost.kt
@@ -7,11 +7,17 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
+import com.aloe_droid.ongi.ui.navigation.NavUtil.Companion.safeMove
 import com.aloe_droid.presentation.base.view.UiContract
+import com.aloe_droid.presentation.filtered_store.contract.FilteredStore
+import com.aloe_droid.presentation.filtered_store.data.StoreFilter
+import com.aloe_droid.presentation.filtered_store.filteredStoreScreen
+import com.aloe_droid.presentation.home.data.LocationData
 import com.aloe_droid.presentation.home.homeScreen
 import com.aloe_droid.presentation.map.contract.route.mapScreen
 import com.aloe_droid.presentation.search.contract.route.searchScreen
 import com.aloe_droid.presentation.setting.contract.route.settingScreen
+import java.util.UUID
 
 @Composable
 fun OnGiNavHost(
@@ -24,15 +30,32 @@ fun OnGiNavHost(
         navController = navController,
         startDestination = startRoute,
         modifier = modifier,
-        enterTransition= { EnterTransition.None },
+        enterTransition = { EnterTransition.None },
         exitTransition = { ExitTransition.None }
     ) {
-        homeScreen(showSnackMessage = showSnackMessage)
+        homeScreen(
+            showSnackMessage = showSnackMessage,
+            navigateToFilteredStore = { filter: StoreFilter, location: LocationData ->
+                navController.safeMove {
+                    val filteredStore = FilteredStore(storeFilter = filter)
+                    navigate(route = filteredStore)
+                }
+            }
+        )
 
         searchScreen()
 
         mapScreen()
 
         settingScreen()
+
+        filteredStoreScreen(
+            showSnackMessage = showSnackMessage,
+            navigateToStore = { id: UUID ->
+                navController.safeMove {
+
+                }
+            }
+        )
     }
 }
