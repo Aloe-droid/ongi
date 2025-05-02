ad3635521aa601cfaf31991ba69cf5c0e8625089
Aloe-droid <https://github.com/Aloe-droid>
2025-05-02 21:07:41 +0900

    feat: Presentation Store Screen 구현

---
+++ presentation/src/main/java/com/aloe_droid/presentation/store/StoreNavigation.kt
@@ -0,0 +1,96 @@
+package com.aloe_droid.presentation.store
+
+import android.content.Context
+import android.content.Intent
+import androidx.compose.material3.SnackbarVisuals
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.core.net.toUri
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.compose.composable
+import com.aloe_droid.presentation.R
+import com.aloe_droid.presentation.base.component.LoadingScreen
+import com.aloe_droid.presentation.base.view.BaseSnackBarVisuals
+import com.aloe_droid.presentation.base.view.CollectSideEffects
+import com.aloe_droid.presentation.store.contract.Store
+import com.aloe_droid.presentation.store.contract.StoreEffect
+import com.aloe_droid.presentation.store.contract.StoreEvent
+import com.aloe_droid.presentation.store.contract.StoreUiState
+import com.aloe_droid.presentation.store.data.AddressData
+import kotlinx.serialization.InternalSerializationApi
+
+@OptIn(InternalSerializationApi::class)
+fun NavGraphBuilder.storeScreen(
+    showSnackMessage: (SnackbarVisuals) -> Unit,
+    navigateUp: () -> Unit,
+) = composable<Store> {
+    val context: Context = LocalContext.current
+    val viewModel: StoreViewModel = hiltViewModel()
+    val uiState: StoreUiState by viewModel.uiState.collectAsStateWithLifecycle()
+
+    CollectSideEffects(effectFlow = viewModel.uiEffect) { sideEffect: StoreEffect ->
+        when (sideEffect) {
+            is StoreEffect.ShowErrorMessage -> {
+                val snackBarVisuals = BaseSnackBarVisuals(message = sideEffect.message)
+                showSnackMessage(snackBarVisuals)
+            }
+
+            is StoreEffect.PopUpWithMessage -> {
+                val snackBarVisuals = BaseSnackBarVisuals(message = sideEffect.message)
+                showSnackMessage(snackBarVisuals)
+                navigateUp()
+            }
+
+            is StoreEffect.MoveToCall -> {
+                val intent = Intent(Intent.ACTION_DIAL).apply {
+                    data = "tel:${sideEffect.phone}".toUri()
+                }
+                context.startActivity(intent)
+            }
+
+            is StoreEffect.MoveToMap -> {
+                val intent = Intent(Intent.ACTION_VIEW).apply {
+                    data = sideEffect.address.toUri()
+                }
+                context.startActivity(intent)
+            }
+
+        }
+    }
+
+    Screen(uiState = uiState, viewModel = viewModel)
+}
+
+@Composable
+private fun Screen(
+    uiState: StoreUiState,
+    viewModel: StoreViewModel
+) {
+    if (uiState.isInitialState) {
+        viewModel.sendEvent(event = StoreEvent.LoadEvent)
+        LoadingScreen(content = stringResource(id = R.string.loading))
+
+    } else if (uiState.store != null) {
+        StoreScreen(
+            storeData = uiState.store,
+            onCallClick = { phone: String ->
+                val event = StoreEvent.CallEvent(phone = phone)
+                viewModel.sendEvent(event = event)
+            },
+            onAddressClick = { address: AddressData ->
+                val event = StoreEvent.MapEvent(address = address)
+                viewModel.sendEvent(event = event)
+            }
+        )
+
+    } else {
+        val message: String = stringResource(id = R.string.cant_find_store)
+        val event: StoreEvent = StoreEvent.CantFindStoreEvent(message = message)
+        viewModel.sendEvent(event = event)
+
+    }
+}
---
+++ presentation/src/main/java/com/aloe_droid/presentation/store/StoreScreen.kt
@@ -0,0 +1,46 @@
+package com.aloe_droid.presentation.store
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.aloe_droid.presentation.base.theme.LargePadding
+import com.aloe_droid.presentation.store.component.MenuListInfo
+import com.aloe_droid.presentation.store.component.OpenExternalApp
+import com.aloe_droid.presentation.store.component.StoreDetailInfo
+import com.aloe_droid.presentation.store.component.StoreTitleInfo
+import com.aloe_droid.presentation.store.data.AddressData
+import com.aloe_droid.presentation.store.data.StoreData
+
+@Composable
+fun StoreScreen(
+    modifier: Modifier = Modifier,
+    storeData: StoreData,
+    onCallClick : (String) -> Unit,
+    onAddressClick: (AddressData) -> Unit
+) {
+    val scrollState = rememberScrollState()
+    Column(
+        modifier = modifier.verticalScroll(state = scrollState),
+        verticalArrangement = Arrangement.spacedBy(LargePadding)
+    ) {
+
+        StoreTitleInfo(storeData = storeData)
+
+        MenuListInfo(storeData = storeData)
+
+        StoreDetailInfo(storeData = storeData)
+
+        OpenExternalApp(
+            storeData = storeData,
+            onCallClick = onCallClick,
+            onAddressClick = onAddressClick
+        )
+
+        Spacer(modifier = Modifier.height(height = LargePadding))
+    }
+}
