06a2ad6dfe3d27509dc075e943219dc8ae852ad6
Aloe-droid <https://github.com/Aloe-droid>
2025-05-02 20:55:05 +0900

    feat: Data 가게 및 사용자 좋아요 API 정의

--- data/src/main/java/com/aloe_droid/data/datasource/network/api/UserAPI.kt
+++ data/src/main/java/com/aloe_droid/data/datasource/network/api/UserAPI.kt
@@ -2,12 +2,15 @@ package com.aloe_droid.data.datasource.network.api
 
 import com.aloe_droid.data.datasource.dto.user.AddressDTO
 import com.aloe_droid.data.datasource.dto.user.IdDTO
+import com.aloe_droid.data.datasource.dto.user.LikeDTO
 import com.aloe_droid.data.datasource.dto.user.UserDTO
 import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.DELETE
+import retrofit2.http.GET
 import retrofit2.http.POST
 import retrofit2.http.Path
+import java.util.UUID
 
 interface UserAPI {
 
@@ -16,5 +19,17 @@ interface UserAPI {
 
     @DELETE("api/v1/users/{id}")
     suspend fun deleteUser(@Path(value = "id") idDTO: IdDTO): Response<Boolean>
+
+    @GET("api/v1/users/{userId}/favorites/{storeId}")
+    suspend fun getStoreLike(
+        @Path(value = "userId") userId: UUID,
+        @Path(value = "storeId") storeId: UUID,
+    ): Response<LikeDTO>
+
+    @POST("api/v1/users/{userId}/favorites/{storeId}")
+    suspend fun toggleStoreLike(
+        @Path(value = "userId") userId: UUID,
+        @Path(value = "storeId") storeId: UUID,
+    ): Response<LikeDTO>
 
 }
