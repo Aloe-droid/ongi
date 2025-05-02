3e0b3afb2ad264b627e644c5a710ab271bd11fbb
Aloe-droid <https://github.com/Aloe-droid>
2025-05-02 20:52:44 +0900

    feat: Domain 가게 정보 repository 정의

--- domain/src/main/java/com/aloe_droid/domain/repository/StoreRepository.kt
+++ domain/src/main/java/com/aloe_droid/domain/repository/StoreRepository.kt
@@ -1,12 +1,21 @@
 package com.aloe_droid.domain.repository
 
 import androidx.paging.PagingData
+import com.aloe_droid.domain.entity.Menu
 import com.aloe_droid.domain.entity.Store
+import com.aloe_droid.domain.entity.StoreDetail
 import com.aloe_droid.domain.entity.StoreQuery
 import kotlinx.coroutines.flow.Flow
+import java.util.UUID
 
 interface StoreRepository {
     fun getStoreList(storeQuery: StoreQuery): Flow<List<Store>>
 
-    fun getStoreStream(storeQuery: StoreQuery) : Flow<PagingData<Store>>
+    fun getStoreStream(storeQuery: StoreQuery): Flow<PagingData<Store>>
+
+    fun getStore(id: UUID, latitude: Double, longitude: Double) : Flow<Store>
+
+    fun getStoreDetail(id: UUID): Flow<StoreDetail>
+
+    fun getStoreMenus(id: UUID): Flow<List<Menu>>
 }
--- domain/src/main/java/com/aloe_droid/domain/repository/UserStoreRepository.kt
+++ domain/src/main/java/com/aloe_droid/domain/repository/UserStoreRepository.kt
@@ -1,12 +1,13 @@
 package com.aloe_droid.domain.repository
 
 import com.aloe_droid.domain.entity.User
+import kotlinx.coroutines.flow.Flow
 
 interface UserStoreRepository {
 
     suspend fun saveUser(user: User)
 
-    suspend fun getUser(): User?
+    fun getUser(): Flow<User?>
 
     suspend fun clearUser()
 
