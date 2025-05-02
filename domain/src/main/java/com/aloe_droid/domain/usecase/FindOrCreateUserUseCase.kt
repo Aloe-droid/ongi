bc340dda972a565512ee91bee1db2641d7dd75ae
Aloe-droid <https://github.com/Aloe-droid>
2025-05-02 20:53:11 +0900

    feat: Domain 사용자 위치 정보 local 저장

--- domain/src/main/java/com/aloe_droid/domain/usecase/FindOrCreateUserUseCase.kt
+++ domain/src/main/java/com/aloe_droid/domain/usecase/FindOrCreateUserUseCase.kt
@@ -3,9 +3,9 @@ package com.aloe_droid.domain.usecase
 import com.aloe_droid.domain.entity.User
 import com.aloe_droid.domain.repository.UserRepository
 import com.aloe_droid.domain.repository.UserStoreRepository
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.emitAll
-import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flatMapLatest
 import kotlinx.coroutines.flow.map
 import java.util.UUID
 import javax.inject.Inject
@@ -14,12 +14,12 @@ class FindOrCreateUserUseCase @Inject constructor(
     private val userStoreRepository: UserStoreRepository,
     private val userRepository: UserRepository
 ) {
-    operator fun invoke(): Flow<Unit> = flow {
-        val savedUser: User? = userStoreRepository.getUser()
-        val userAddress: String = savedUser?.address ?: UUID.randomUUID().toString()
-        val flow: Flow<Unit> = userRepository.findOrCreateUser(userAddress).map { user: User ->
-            userStoreRepository.saveUser(user)
-        }
-        emitAll(flow)
+    @OptIn(ExperimentalCoroutinesApi::class)
+    operator fun invoke(): Flow<Unit> = userStoreRepository.getUser().map { savedUser: User? ->
+        savedUser?.address ?: UUID.randomUUID().toString()
+    }.flatMapLatest { address: String ->
+        userRepository.findOrCreateUser(address = address)
+    }.map { user: User ->
+        userStoreRepository.saveUser(user)
     }
 }
