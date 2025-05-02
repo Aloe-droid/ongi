19d0dd62a7d1794aeafcc8b11792190d02aed718
Aloe-droid <https://github.com/Aloe-droid>
2025-05-02 21:02:11 +0900

    feat: Data 가게 Mapper 정의

--- data/src/main/java/com/aloe_droid/data/repository/mapper/StoreMapper.kt
+++ data/src/main/java/com/aloe_droid/data/repository/mapper/StoreMapper.kt
@@ -1,7 +1,11 @@
 package com.aloe_droid.data.repository.mapper
 
+import com.aloe_droid.data.datasource.dto.store.MenuDTO
 import com.aloe_droid.data.datasource.dto.store.StoreDTO
+import com.aloe_droid.data.datasource.dto.store.StoreDetailDTO
+import com.aloe_droid.domain.entity.Menu
 import com.aloe_droid.domain.entity.Store
+import com.aloe_droid.domain.entity.StoreDetail
 
 object StoreMapper {
     fun List<StoreDTO>.toStoreList(): List<Store> = map { it.toStore() }
@@ -19,5 +23,25 @@ object StoreMapper {
         city = city,
         district = district,
         imageUrl = imageUrl
+    )
+
+    fun StoreDetailDTO.toStoreDetail() = StoreDetail(
+        hasParking = hasParking,
+        hasTakeout = hasTakeout,
+        hasDelivery = hasDelivery,
+        hasReservation = hasReservation,
+        hasDividedRestroom = hasDividedRestroom,
+        allowsPets = allowsPets,
+        hasWifi = hasWifi,
+        hasKidsFacility = hasKidsFacility,
+        allowsGroup = allowsGroup
+    )
+
+    fun List<MenuDTO>.toMenuList(): List<Menu> = map { it.toMenu() }
+
+    fun MenuDTO.toMenu() = Menu(
+        id = id,
+        name = name,
+        price = price
     )
 }
