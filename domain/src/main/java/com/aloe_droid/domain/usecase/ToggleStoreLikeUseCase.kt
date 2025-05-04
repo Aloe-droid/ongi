f6c4e81c5dc449357c55248fa9adc69215584fff
Aloe-droid <https://github.com/Aloe-droid>
2025-05-04 22:01:47 +0900

    feat: Domain 가게 즐겨찾기 및 동기화 UseCase 정의

--- domain/src/main/java/com/aloe_droid/domain/usecase/ToggleStoreLikeUseCase.kt
+++ domain/src/main/java/com/aloe_droid/domain/usecase/ToggleStoreLikeUseCase.kt
@@ -1,18 +1,21 @@
 package com.aloe_droid.domain.usecase
 
 import com.aloe_droid.domain.entity.User
+import com.aloe_droid.domain.repository.StoreRepository
 import com.aloe_droid.domain.repository.UserRepository
 import com.aloe_droid.domain.repository.UserStoreRepository
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.filterNotNull
 import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.onEach
 import java.util.UUID
 import javax.inject.Inject
 
 class ToggleStoreLikeUseCase @Inject constructor(
     private val userRepository: UserRepository,
-    private val userStoreRepository: UserStoreRepository
+    private val userStoreRepository: UserStoreRepository,
+    private val storeRepository: StoreRepository,
 ) {
 
     @OptIn(ExperimentalCoroutinesApi::class)
@@ -21,6 +24,7 @@ class ToggleStoreLikeUseCase @Inject constructor(
         .flatMapLatest { user: User ->
             val userId: UUID = user.id
             userRepository.toggleStoreLike(userId = userId, storeId = storeId)
+        }.onEach { isLike: Boolean ->
+            storeRepository.updateStoreFavoriteCount(id = storeId, isLike = isLike)
         }
-
 }
