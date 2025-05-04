b863c6bdb0902bff8b0fe122b3f8c5f5bddadf1b
Aloe-droid <https://github.com/Aloe-droid>
2025-05-04 22:19:20 +0900

    feat: Presentation Search 화면 구현

---
+++ presentation/src/main/java/com/aloe_droid/presentation/search/SearchNavigation.kt
@@ -0,0 +1,96 @@
+package com.aloe_droid.presentation.search
+
+import androidx.compose.material3.SnackbarVisuals
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.res.stringResource
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.navigation.NavBackStackEntry
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.compose.composable
+import androidx.paging.compose.LazyPagingItems
+import androidx.paging.compose.collectAsLazyPagingItems
+import com.aloe_droid.domain.entity.SearchHistory
+import com.aloe_droid.presentation.R
+import com.aloe_droid.presentation.base.component.LoadingScreen
+import com.aloe_droid.presentation.base.view.BaseSnackBarVisuals
+import com.aloe_droid.presentation.base.view.CollectSideEffects
+import com.aloe_droid.presentation.search.contract.Search
+import com.aloe_droid.presentation.search.contract.SearchEffect
+import com.aloe_droid.presentation.search.contract.SearchEvent
+import com.aloe_droid.presentation.search.contract.SearchUiState
+import com.aloe_droid.presentation.search.data.SearchedStore
+import java.util.UUID
+
+fun NavGraphBuilder.searchScreen(
+    showSnackMessage: (SnackbarVisuals) -> Unit,
+    navigateToStore: (UUID) -> Unit,
+    navigateToFilteredStore: (String) -> Unit,
+    navigateUp: () -> Unit,
+) = composable<Search> { backStackEntry: NavBackStackEntry ->
+    val fromBottomNavi: Boolean? = backStackEntry.arguments?.getBoolean(Search.iS_BOTTOM)
+    val viewModel: SearchViewModel = hiltViewModel()
+    val uiState: SearchUiState by viewModel.uiState.collectAsStateWithLifecycle()
+    val storeItems: LazyPagingItems<SearchedStore> = viewModel.queryResult
+        .collectAsLazyPagingItems()
+    val historyItems: LazyPagingItems<SearchHistory> = viewModel.searchHistory
+        .collectAsLazyPagingItems()
+
+    CollectSideEffects(effectFlow = viewModel.uiEffect) { sideEffect: SearchEffect ->
+        when (sideEffect) {
+            is SearchEffect.ShowErrorMessage -> {
+                val snackBarVisuals = BaseSnackBarVisuals(message = sideEffect.message)
+                showSnackMessage(snackBarVisuals)
+            }
+
+            is SearchEffect.SelectStore -> {
+                navigateToStore(sideEffect.storeId)
+            }
+
+            is SearchEffect.NavigateToFilteredStoreList -> {
+                navigateToFilteredStore(sideEffect.query)
+            }
+
+            is SearchEffect.NavigateUp -> {
+                navigateUp()
+            }
+        }
+    }
+
+    if (uiState.isInitialState) {
+        viewModel.sendEvent(event = SearchEvent.LoadEvent)
+        LoadingScreen(content = stringResource(id = R.string.loading))
+    } else {
+        SearchScreen(
+            canGoUp = fromBottomNavi == false,
+            storeItems = storeItems,
+            historyItems = historyItems,
+            query = uiState.searchQuery,
+            navigateUp = {
+                val event = SearchEvent.NavigateUpEvent
+                viewModel.sendEvent(event = event)
+            },
+            onQueryChange = { query: String ->
+                val event = SearchEvent.ChangeQuery(query = query)
+                viewModel.sendEvent(event = event)
+            },
+            onSearch = { query: String ->
+                if (query.isBlank()) return@SearchScreen
+                val event = SearchEvent.SearchQuery(query = query)
+                viewModel.sendEvent(event = event)
+            },
+            selectStore = { storeId: UUID ->
+                val event = SearchEvent.SelectStore(id = storeId)
+                viewModel.sendEvent(event = event)
+            },
+            deleteHistory = { historyId: Long ->
+                val event = SearchEvent.DeleteQuery(id = historyId)
+                viewModel.sendEvent(event = event)
+            },
+            deleteAllHistory = {
+                val event = SearchEvent.DeleteAllQuery
+                viewModel.sendEvent(event = event)
+            }
+        )
+    }
+}
--- presentation/src/main/java/com/aloe_droid/presentation/search/SearchScreen.kt
+++ presentation/src/main/java/com/aloe_droid/presentation/search/SearchScreen.kt
@@ -1,15 +1,62 @@
 package com.aloe_droid.presentation.search
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.paging.compose.LazyPagingItems
+import com.aloe_droid.domain.entity.SearchHistory
+import com.aloe_droid.presentation.base.theme.DefaultPadding
+import com.aloe_droid.presentation.base.theme.SemiLargePadding
+import com.aloe_droid.presentation.base.theme.SmallPadding
+import com.aloe_droid.presentation.search.component.Histories
+import com.aloe_droid.presentation.search.component.OnGiSearchBar
+import com.aloe_droid.presentation.search.data.SearchedStore
+import java.util.UUID
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun SearchScreen() {
-    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-        Text("여기는 검색")
+fun SearchScreen(
+    canGoUp: Boolean,
+    storeItems: LazyPagingItems<SearchedStore>,
+    historyItems: LazyPagingItems<SearchHistory>,
+    query: String,
+    navigateUp: () -> Unit,
+    onQueryChange: (String) -> Unit,
+    onSearch: (String) -> Unit,
+    selectStore: (UUID) -> Unit,
+    deleteHistory: (Long) -> Unit,
+    deleteAllHistory: () -> Unit
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = SemiLargePadding)
+    ) {
+        OnGiSearchBar(
+            canGoUp = canGoUp,
+            navigateUp = navigateUp,
+            query = query,
+            onQueryChange = onQueryChange,
+            storeItems = storeItems,
+            selectStore = selectStore,
+            onSearch = onSearch
+        )
+
+        Spacer(modifier = Modifier.height(DefaultPadding))
+
+        Histories(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = SmallPadding),
+            historyItems = historyItems,
+            deleteHistory = deleteHistory,
+            onSearch = onSearch,
+            deleteAllHistory = deleteAllHistory
+        )
     }
 }
