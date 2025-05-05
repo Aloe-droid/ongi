f890ceb64889398a4d86cb6d11b707775872b951
Aloe-droid <https://github.com/Aloe-droid>
2025-05-06 00:34:17 +0900

    refactor: Domain User 정보 기반 쿼리 UseCase 수정

--- domain/src/main/java/com/aloe_droid/domain/usecase/GetFilteredStoreUseCase.kt
+++ domain/src/main/java/com/aloe_droid/domain/usecase/GetFilteredStoreUseCase.kt
@@ -1,33 +1,66 @@
 package com.aloe_droid.domain.usecase
 
-import androidx.paging.PagingData
 import com.aloe_droid.domain.entity.Location
-import com.aloe_droid.domain.entity.Store
 import com.aloe_droid.domain.entity.StoreQuery
+import com.aloe_droid.domain.entity.StoreQueryCategory
+import com.aloe_droid.domain.entity.StoreQueryDistance
+import com.aloe_droid.domain.entity.StoreQuerySortType
+import com.aloe_droid.domain.entity.User
 import com.aloe_droid.domain.repository.LocationRepository
 import com.aloe_droid.domain.repository.StoreRepository
+import com.aloe_droid.domain.repository.UserStoreRepository
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.filterNotNull
 import kotlinx.coroutines.flow.flatMapLatest
 import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 class GetFilteredStoreUseCase @Inject constructor(
+    private val userStoreRepository: UserStoreRepository,
     private val locationRepository: LocationRepository,
     private val storeRepository: StoreRepository
 ) {
 
     @OptIn(ExperimentalCoroutinesApi::class)
-    operator fun invoke(storeQuery: StoreQuery): Flow<PagingData<Store>> = locationRepository
-        .getLocalLocation()
-        .map { location: Location -> storeQuery.copy(location = location) }
-        .flatMapLatest { storeRepository.getStoreStream(it) }
-
+    operator fun invoke(
+        category: StoreQueryCategory,
+        sortType: StoreQuerySortType,
+        distanceRange: StoreQueryDistance,
+        searchQuery: String,
+        onlyFavorites: Boolean
+    ) = combine(
+        userStoreRepository.getUser(),
+        locationRepository.getLocation()
+    ) { user: User?, location: Location ->
+        if (user == null) null
+        else user to location
+    }.filterNotNull()
+        .map { (user: User, location: Location) ->
+            StoreQuery(
+                userId = user.id,
+                location = location,
+                searchQuery = searchQuery,
+                category = category,
+                sortType = sortType,
+                distanceRange = distanceRange,
+                onlyFavorites = onlyFavorites
+            )
+        }.flatMapLatest { storeQuery ->
+            storeRepository.getStoreStream(storeQuery)
+        }
 
     @OptIn(ExperimentalCoroutinesApi::class)
-    operator fun invoke(searchQuery: String): Flow<PagingData<Store>> = locationRepository
-        .getLocalLocation()
-        .map { location: Location -> StoreQuery(location = location, searchQuery = searchQuery) }
-        .flatMapLatest { storeRepository.getStoreStream(it) }
-
+    operator fun invoke(searchQuery: String) = combine(
+        userStoreRepository.getUser(),
+        locationRepository.getLocation()
+    ) { user: User?, location: Location ->
+        if (user == null) null
+        else user to location
+    }.filterNotNull()
+        .map { (user: User, location: Location) ->
+            StoreQuery(userId = user.id, location = location, searchQuery = searchQuery)
+        }.flatMapLatest { storeQuery ->
+            storeRepository.getStoreStream(storeQuery)
+        }
 }
--- domain/src/main/java/com/aloe_droid/domain/usecase/GetHomeInfoUseCase.kt
+++ domain/src/main/java/com/aloe_droid/domain/usecase/GetHomeInfoUseCase.kt
@@ -1,32 +1,42 @@
 package com.aloe_droid.domain.usecase
 
-import com.aloe_droid.domain.entity.Banner
 import com.aloe_droid.domain.entity.HomeEntity
 import com.aloe_droid.domain.entity.Location
 import com.aloe_droid.domain.entity.Store
 import com.aloe_droid.domain.entity.StoreQuery
 import com.aloe_droid.domain.entity.StoreQuerySortType
+import com.aloe_droid.domain.entity.User
 import com.aloe_droid.domain.repository.BannerRepository
 import com.aloe_droid.domain.repository.LocationRepository
 import com.aloe_droid.domain.repository.StoreRepository
+import com.aloe_droid.domain.repository.UserStoreRepository
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.filterNotNull
 import kotlinx.coroutines.flow.flatMapLatest
+import java.util.UUID
 import javax.inject.Inject
 
 class GetHomeInfoUseCase @Inject constructor(
-    private val bannerRepository: BannerRepository,
+    private val userStoreRepository: UserStoreRepository,
     private val locationRepository: LocationRepository,
+    private val bannerRepository: BannerRepository,
     private val storeRepository: StoreRepository,
 ) {
 
     @OptIn(ExperimentalCoroutinesApi::class)
-    operator fun invoke(): Flow<HomeEntity> {
-        return locationRepository.getLocation().flatMapLatest { location: Location ->
-            val bannerListFlow: Flow<List<Banner>> = bannerRepository.getBannerList()
-            val favoriteStoreListFlow: Flow<List<Store>> = getFavoriteStoreList(location = location)
-            val nearbyStoreListFlow: Flow<List<Store>> = getNearbyStoreList(location = location)
+    operator fun invoke(): Flow<HomeEntity> = combine(
+        userStoreRepository.getUser(),
+        locationRepository.getLocation()
+    ) { user: User?, location: Location ->
+        if (user == null) null
+        else user to location
+    }.filterNotNull()
+        .flatMapLatest { (user: User, location: Location) ->
+            val bannerListFlow = bannerRepository.getBannerList()
+            val favoriteStoreListFlow = getFavoriteStoreList(location = location, userId = user.id)
+            val nearbyStoreListFlow = getNearbyStoreList(location = location, userId = user.id)
 
             combine(
                 bannerListFlow,
@@ -41,10 +51,10 @@ class GetHomeInfoUseCase @Inject constructor(
                 )
             }
         }
-    }
 
-    private fun getNearbyStoreList(location: Location): Flow<List<Store>> {
+    private fun getNearbyStoreList(location: Location, userId: UUID): Flow<List<Store>> {
         val query = StoreQuery(
+            userId = userId,
             location = Location(latitude = location.latitude, longitude = location.longitude),
             sortType = StoreQuerySortType.DISTANCE,
             size = HOME_STORE_SIZE
@@ -53,8 +63,9 @@ class GetHomeInfoUseCase @Inject constructor(
         return storeRepository.getStoreList(query)
     }
 
-    private fun getFavoriteStoreList(location: Location): Flow<List<Store>> {
+    private fun getFavoriteStoreList(location: Location, userId: UUID): Flow<List<Store>> {
         val query = StoreQuery(
+            userId = userId,
             location = Location(latitude = location.latitude, longitude = location.longitude),
             sortType = StoreQuerySortType.FAVORITE,
             size = HOME_STORE_SIZE
@@ -67,3 +78,4 @@ class GetHomeInfoUseCase @Inject constructor(
         private const val HOME_STORE_SIZE: Int = 10
     }
 }
+
