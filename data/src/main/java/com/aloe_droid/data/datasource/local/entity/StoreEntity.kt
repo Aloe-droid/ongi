f7bd8465e7ef0cbaf4a87a68e86eeddcc09dc89a
Aloe-droid <https://github.com/Aloe-droid>
2025-05-06 21:57:24 +0900

    feat: Data Store 엔티티 수정 (페이지 넘버 및 불필요한 데이터 제거)

--- data/src/main/java/com/aloe_droid/data/datasource/local/entity/StoreEntity.kt
+++ data/src/main/java/com/aloe_droid/data/datasource/local/entity/StoreEntity.kt
@@ -1,14 +1,14 @@
 package com.aloe_droid.data.datasource.local.entity
 
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.aloe_droid.data.datasource.dto.store.StoreDTO
 import com.aloe_droid.domain.entity.Store
 import java.util.UUID
 
-@Entity(tableName = "stores", primaryKeys = ["id", "queryId"])
+@Entity(tableName = "stores")
 data class StoreEntity(
-    val id: String,
-    val queryId: String, // TODO("추후에 쿼리 별로 DB 캐시 정책")
+    @PrimaryKey val id: String,
     val name: String,
     val address: String,
     val favoriteCount: Int,
@@ -19,24 +19,18 @@ data class StoreEntity(
     val city: String,
     val district: String,
     val phone: String?,
-    val imageUrl: String?,
-
-    val pageNumber: Int,
-    val orderIndex: Int,
+    val imageUrl: String?
 ) {
     companion object {
-        fun List<StoreDTO>.toStoreEntityList(
-            pageNumber: Int,
-            queryId: String,
-        ) = mapIndexed { index, store ->
-            store.toStoreEntity(pageNumber = pageNumber, orderIndex = index, queryId = queryId)
+        fun List<StoreDTO>.toStoreEntityList() = map { store ->
+            store.toStoreEntity()
         }
 
-        fun StoreDTO.toStoreEntity(
-            pageNumber: Int,
-            orderIndex: Int,
-            queryId: String,
-        ) = StoreEntity(
+        fun List<StoreEntity>.toStoreList() = map { storeEntity ->
+            storeEntity.toStore()
+        }
+
+        fun StoreDTO.toStoreEntity() = StoreEntity(
             id = id.toString(),
             name = name,
             address = address,
@@ -49,10 +43,6 @@ data class StoreEntity(
             district = district,
             phone = phone,
             imageUrl = imageUrl,
-
-            pageNumber = pageNumber,
-            orderIndex = orderIndex,
-            queryId = queryId,
         )
 
         fun StoreEntity.toStore() = Store(
