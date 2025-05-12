8b40b56a53ed3d4576564181f86f3313e4c58c56
Aloe-droid <https://github.com/Aloe-droid>
2025-05-12 17:01:04 +0900

    feat: 다크모드 색상 정의

--- app/src/main/java/com/aloe_droid/ongi/ui/theme/Color.kt
+++ app/src/main/java/com/aloe_droid/ongi/ui/theme/Color.kt
@@ -2,11 +2,6 @@ package com.aloe_droid.ongi.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Pink80 = Color(0xFFEFB8C8)
-
-val SelectColor: Color = Color.Black
-val UnSelectColor: Color = Color.Gray
-
 val LightPrimary = Color(0xFF519BFF)
 val LightSecondaryPrimary = Color(0xFFD0E4FF)
 
