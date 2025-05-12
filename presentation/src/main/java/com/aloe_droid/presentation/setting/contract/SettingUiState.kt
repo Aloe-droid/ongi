112c9f0cf9999366cd4d9a1c1ae90bfc2e7f28dc
Aloe-droid <https://github.com/Aloe-droid>
2025-05-12 16:24:47 +0900

    feat: Presentation Setting Load 이벤트 삭제, uiData 분리

--- presentation/src/main/java/com/aloe_droid/presentation/setting/contract/SettingUiState.kt
+++ presentation/src/main/java/com/aloe_droid/presentation/setting/contract/SettingUiState.kt
@@ -1,12 +1,24 @@
 package com.aloe_droid.presentation.setting.contract
 
 import androidx.compose.runtime.Stable
+import com.aloe_droid.domain.entity.StoreSyncEntity
 import com.aloe_droid.presentation.base.view.UiContract
 import kotlinx.datetime.Instant
 
 @Stable
 data class SettingUiState(
-    val isInitialState: Boolean = true,
+    val isInitialState: Boolean = true
+) : UiContract.State
+
+@Stable
+data class SettingUiData(
     val storeCount: Long = 0L,
     val syncTime: Instant = Instant.fromEpochMilliseconds(0L)
-) : UiContract.State
+) {
+    companion object {
+        fun StoreSyncEntity.toSettingData() = SettingUiData(
+            storeCount = storeCount,
+            syncTime = syncTime
+        )
+    }
+}
