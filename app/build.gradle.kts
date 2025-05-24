d1451cf03027246cf620f9be4f3a30935b7427ac
Aloe-droid <103430477+Aloe-droid@users.noreply.github.com>
2025-05-25 00:21:37 +0900

    Merge pull request #21
    * feat: Presentation Wc 분리 표시 된 벡터 추가
    
    * fix: Presentation 잘못된 매핑 관계 수정
    
    * chore: 버전 upgrade

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.aloe_droid.ongi"
         minSdk = 24
         targetSdk = 35
-        versionCode = 5
-        versionName = "1.0.2"
+        versionCode = 6
+        versionName = "1.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("String", "CLIENT_ID", properties.getProperty("nClientId"))
--- presentation/src/main/java/com/aloe_droid/presentation/store/data/StoreDetailData.kt
+++ presentation/src/main/java/com/aloe_droid/presentation/store/data/StoreDetailData.kt
@@ -8,12 +8,12 @@ import androidx.compose.material.icons.outlined.Groups2
 import androidx.compose.material.icons.outlined.LocalParking
 import androidx.compose.material.icons.outlined.Pets
 import androidx.compose.material.icons.outlined.ShoppingBag
-import androidx.compose.material.icons.outlined.Wc
 import androidx.compose.material.icons.outlined.Wifi
 import androidx.compose.runtime.Stable
 import androidx.compose.ui.graphics.vector.ImageVector
 import com.aloe_droid.domain.entity.StoreDetail
 import com.aloe_droid.presentation.R
+import com.aloe_droid.presentation.store.component.WcSeparated
 
 @Stable
 data class StoreDetailData(
@@ -45,11 +45,11 @@ data class StoreDetailData(
             R.string.take_out to (hasTakeout to Icons.Outlined.ShoppingBag),
             R.string.delivery to (hasDelivery to Icons.Outlined.DeliveryDining),
             R.string.reservation to (hasReservation to Icons.Outlined.AccessTime),
-            R.string.dividedRestroom to (hasDividedRestroom to Icons.Outlined.Wc),
-            R.string.allows_group to (allowsGroup to Icons.Outlined.Groups2),
+            R.string.dividedRestroom to (hasDividedRestroom to Icons.Outlined.WcSeparated),
+            R.string.allows_group to (allowsGroup to Icons.Outlined.ChildCare),
             R.string.has_wifi to (hasWifi to Icons.Outlined.Wifi),
-            R.string.allows_pets to (allowsPets to Icons.Outlined.Pets),
-            R.string.has_kids_facility to (hasKidsFacility to Icons.Outlined.ChildCare)
+            R.string.allows_pets to (allowsPets to Icons.Outlined.Groups2),
+            R.string.has_kids_facility to (hasKidsFacility to Icons.Outlined.Pets)
         )
     }
 }
